cfcc4d30799bc3faccfd12489e5c3a2ca3d1bd89
GlutenFreeGrapes <geobee2004@gmail.com>
2024-01-31 15:26:52 -0500

    Fixed population bug(?)
    Added function such that if the population of the first result is too small, results are resorted to sort by population first. This means that Ghent actually returns Gent, Belgium, and Acapulco returns Acapulco de Juarez, Mexico, but small towns like Okaihau will return the original result.

--- citieschain.py
+++ citieschain.py
@@ -123,7 +123,7 @@ iso2={'AD': 'Andorra', 'AE': 'United Arab Emirates', 'AF': 'Afghanistan', 'AG': 
 iso3={'AND': 'Andorra', 'ARE': 'United Arab Emirates', 'AFG': 'Afghanistan', 'ATG': 'Antigua and Barbuda', 'AIA': 'Anguilla', 'ALB': 'Albania', 'ARM': 'Armenia', 'AGO': 'Angola', 'ATA': 'Antarctica', 'ARG': 'Argentina', 'ASM': 'American Samoa', 'AUT': 'Austria', 'AUS': 'Australia', 'ABW': 'Aruba', 'ALA': 'Aland Islands', 'AZE': 'Azerbaijan', 'BIH': 'Bosnia and Herzegovina', 'BRB': 'Barbados', 'BGD': 'Bangladesh', 'BEL': 'Belgium', 'BFA': 'Burkina Faso', 'BGR': 'Bulgaria', 'BHR': 'Bahrain', 'BDI': 'Burundi', 'BEN': 'Benin', 'BLM': 'Saint Barthelemy', 'BMU': 'Bermuda', 'BRN': 'Brunei', 'BOL': 'Bolivia', 'BES': 'Bonaire, Saint Eustatius and Saba ', 'BRA': 'Brazil', 'BHS': 'Bahamas', 'BTN': 'Bhutan', 'BVT': 'Bouvet Island', 'BWA': 'Botswana', 'BLR': 'Belarus', 'BLZ': 'Belize', 'CAN': 'Canada', 'CCK': 'Cocos Islands', 'COD': 'Democratic Republic of the Congo', 'CAF': 'Central African Republic', 'COG': 'Republic of the Congo', 'CHE': 'Switzerland', 'CIV': 'Ivory Coast', 'COK': 'Cook Islands', 'CHL': 'Chile', 'CMR': 'Cameroon', 'CHN': 'China', 'COL': 'Colombia', 'CRI': 'Costa Rica', 'CUB': 'Cuba', 'CPV': 'Cabo Verde', 'CUW': 'Curacao', 'CXR': 'Christmas Island', 'CYP': 'Cyprus', 'CZE': 'Czechia', 'DEU': 'Germany', 'DJI': 'Djibouti', 'DNK': 'Denmark', 'DMA': 'Dominica', 'DOM': 'Dominican Republic', 'DZA': 'Algeria', 'ECU': 'Ecuador', 'EST': 'Estonia', 'EGY': 'Egypt', 'ESH': 'Western Sahara', 'ERI': 'Eritrea', 'ESP': 'Spain', 'ETH': 'Ethiopia', 'FIN': 'Finland', 'FJI': 'Fiji', 'FLK': 'Falkland Islands', 'FSM': 'Micronesia', 'FRO': 'Faroe Islands', 'FRA': 'France', 'GAB': 'Gabon', 'GBR': 'United Kingdom', 'GRD': 'Grenada', 'GEO': 'Georgia', 'GUF': 'French Guiana', 'GGY': 'Guernsey', 'GHA': 'Ghana', 'GIB': 'Gibraltar', 'GRL': 'Greenland', 'GMB': 'Gambia', 'GIN': 'Guinea', 'GLP': 'Guadeloupe', 'GNQ': 'Equatorial Guinea', 'GRC': 'Greece', 'SGS': 'South Georgia and the South Sandwich Islands', 'GTM': 'Guatemala', 'GUM': 'Guam', 'GNB': 'Guinea-Bissau', 'GUY': 'Guyana', 'HKG': 'Hong Kong', 'HMD': 'Heard Island and McDonald Islands', 'HND': 'Honduras', 'HRV': 'Croatia', 'HTI': 'Haiti', 'HUN': 'Hungary', 'IDN': 'Indonesia', 'IRL': 'Ireland', 'ISR': 'Israel', 'IMN': 'Isle of Man', 'IND': 'India', 'IOT': 'British Indian Ocean Territory', 'IRQ': 'Iraq', 'IRN': 'Iran', 'ISL': 'Iceland', 'ITA': 'Italy', 'JEY': 'Jersey', 'JAM': 'Jamaica', 'JOR': 'Jordan', 'JPN': 'Japan', 'KEN': 'Kenya', 'KGZ': 'Kyrgyzstan', 'KHM': 'Cambodia', 'KIR': 'Kiribati', 'COM': 'Comoros', 'KNA': 'Saint Kitts and Nevis', 'PRK': 'North Korea', 'KOR': 'South Korea', 'XKX': 'Kosovo', 'KWT': 'Kuwait', 'CYM': 'Cayman Islands', 'KAZ': 'Kazakhstan', 'LAO': 'Laos', 'LBN': 'Lebanon', 'LCA': 'Saint Lucia', 'LIE': 'Liechtenstein', 'LKA': 'Sri Lanka', 'LBR': 'Liberia', 'LSO': 'Lesotho', 'LTU': 'Lithuania', 'LUX': 'Luxembourg', 'LVA': 'Latvia', 'LBY': 'Libya', 'MAR': 'Morocco', 'MCO': 'Monaco', 'MDA': 'Moldova', 'MNE': 'Montenegro', 'MAF': 'Saint Martin', 'MDG': 'Madagascar', 'MHL': 'Marshall Islands', 'MKD': 'North Macedonia', 'MLI': 'Mali', 'MMR': 'Myanmar', 'MNG': 'Mongolia', 'MAC': 'Macao', 'MNP': 'Northern Mariana Islands', 'MTQ': 'Martinique', 'MRT': 'Mauritania', 'MSR': 'Montserrat', 'MLT': 'Malta', 'MUS': 'Mauritius', 'MDV': 'Maldives', 'MWI': 'Malawi', 'MEX': 'Mexico', 'MYS': 'Malaysia', 'MOZ': 'Mozambique', 'NAM': 'Namibia', 'NCL': 'New Caledonia', 'NER': 'Niger', 'NFK': 'Norfolk Island', 'NGA': 'Nigeria', 'NIC': 'Nicaragua', 'NLD': 'Netherlands', 'NOR': 'Norway', 'NPL': 'Nepal', 'NRU': 'Nauru', 'NIU': 'Niue', 'NZL': 'New Zealand', 'OMN': 'Oman', 'PAN': 'Panama', 'PER': 'Peru', 'PYF': 'French Polynesia', 'PNG': 'Papua New Guinea', 'PHL': 'Philippines', 'PAK': 'Pakistan', 'POL': 'Poland', 'SPM': 'Saint Pierre and Miquelon', 'PCN': 'Pitcairn', 'PRI': 'Puerto Rico', 'PSE': 'Palestinian Territory', 'PRT': 'Portugal', 'PLW': 'Palau', 'PRY': 'Paraguay', 'QAT': 'Qatar', 'REU': 'Reunion', 'ROU': 'Romania', 'SRB': 'Serbia', 'RUS': 'Russia', 'RWA': 'Rwanda', 'SAU': 'Saudi Arabia', 'SLB': 'Solomon Islands', 'SYC': 'Seychelles', 'SDN': 'Sudan', 'SSD': 'South Sudan', 'SWE': 'Sweden', 'SGP': 'Singapore', 'SHN': 'Saint Helena', 'SVN': 'Slovenia', 'SJM': 'Svalbard and Jan Mayen', 'SVK': 'Slovakia', 'SLE': 'Sierra Leone', 'SMR': 'San Marino', 'SEN': 'Senegal', 'SOM': 'Somalia', 'SUR': 'Suriname', 'STP': 'Sao Tome and Principe', 'SLV': 'El Salvador', 'SXM': 'Sint Maarten', 'SYR': 'Syria', 'SWZ': 'Eswatini', 'TCA': 'Turks and Caicos Islands', 'TCD': 'Chad', 'ATF': 'French Southern Territories', 'TGO': 'Togo', 'THA': 'Thailand', 'TJK': 'Tajikistan', 'TKL': 'Tokelau', 'TLS': 'Timor Leste', 'TKM': 'Turkmenistan', 'TUN': 'Tunisia', 'TON': 'Tonga', 'TUR': 'Turkey', 'TTO': 'Trinidad and Tobago', 'TUV': 'Tuvalu', 'TWN': 'Taiwan', 'TZA': 'Tanzania', 'UKR': 'Ukraine', 'UGA': 'Uganda', 'UMI': 'United States Minor Outlying Islands', 'USA': 'United States', 'URY': 'Uruguay', 'UZB': 'Uzbekistan', 'VAT': 'Vatican', 'VCT': 'Saint Vincent and the Grenadines', 'VEN': 'Venezuela', 'VGB': 'British Virgin Islands', 'VIR': 'U.S. Virgin Islands', 'VNM': 'Vietnam', 'VUT': 'Vanuatu', 'WLF': 'Wallis and Futuna', 'WSM': 'Samoa', 'YEM': 'Yemen', 'MYT': 'Mayotte', 'ZAF': 'South Africa', 'ZMB': 'Zambia', 'ZWE': 'Zimbabwe', 'SCG': 'Serbia and Montenegro', 'ANT': 'Netherlands Antilles'}
 regionalindicators={'a':'🇦','b':'🇧','c':'🇨','d':'🇩','e':'🇪','f':'🇫','g':'🇬','h':'🇭','i':'🇮','j':'🇯','k':'🇰','l':'🇱','m':'🇲','n':'🇳','o':'🇴','p':'🇵','q':'🇶','r':'🇷','s':'🇸','t':'🇹','u':'🇺','v':'🇻','w':'🇼','x':'🇽','y':'🇾','z':'🇿'}
 
-def search_cities(city,province,country,checkApostrophe):
+def search_cities(city,province,country,checkApostrophe,min_pop):
     s=('name','decoded','punct space','punct empty')
     city=re.sub(',$','',city.casefold().strip())
     if city[-1]==',':
@@ -163,14 +163,22 @@ def search_cities(city,province,country,checkApostrophe):
         if checkApostrophe:
             return None
         else:
-            return search_cities(city.replace("`","'").replace("’","'").replace("ʻ","'").replace("ʼ","'"),province,country,1)
+            return search_cities(city.replace("`","'").replace("’","'").replace("ʻ","'").replace("ʼ","'"),province,country,1,min_pop)
     else:
         r=results.sort_values(['default','population','match'],ascending=[0,0,1]).head(1).iloc[0]
+
+        # if population too small, look for larger options. if none, return original result
+        if r['population']<min_pop:
+            alternateresult=results.sort_values(['population','default','match'],ascending=[0,0,1]).head(1).iloc[0]
+            if alternateresult['population']>=min_pop:
+                r=alternateresult
+
+
         return (int(r['geonameid']),r,r[s[r['match']]])
 
 codes={'country code','admin1 code','admin2 code'}
 names={'country names','admin1 names','admin2 names',"alternate countries",'alternate country names'}
-def search_cities_chain(query, checkApostrophe):
+def search_cities_chain(query, checkApostrophe,min_pop):
     s=('name','decoded','punct space','punct empty')
     q=re.sub(',$','',query.casefold().strip())
     if q[-1]==',':
@@ -244,9 +252,17 @@ def search_cities_chain(query, checkApostrophe):
         if checkApostrophe:
             return None
         else:
-            return search_cities_chain(query.replace("`","'").replace("’","'").replace("ʻ","'").replace("ʼ","'"),1)
+            return search_cities_chain(query.replace("`","'").replace("’","'").replace("ʻ","'").replace("ʼ","'"),1,min_pop)
     else:
+
         r=results.sort_values(['default','population','match'],ascending=[0,0,1]).head(1).iloc[0]
+        
+        # if population too small, look for larger options. if none, return original result
+        if r['population']<min_pop:
+            alternateresult=results.sort_values(['population','default','match'],ascending=[0,0,1]).head(1).iloc[0]
+            if alternateresult['population']>=min_pop:
+                r=alternateresult
+
         return (int(r['geonameid']),r,r[s[r['match']]])
     
 class Help(discord.ui.View):
@@ -624,7 +640,11 @@ async def react(interaction: discord.Interaction, city:str, province:Optional[st
     if interaction.user.id in bans:
         await interaction.followup.send(":no_pedestrians: You are blocked from using this bot. ")
         return
-    res=search_cities(city,province,country,0)
+    
+    cur.execute('select min_pop from server_info where server_id=?',data=(interaction.guild_id,))
+    minimum_population = cur.fetchone()[0]
+
+    res=search_cities(city,province,country,0,minimum_population)
     if res:
         await interaction.followup.send('What reaction do you want for **%s**? React to this message with the emoji. '%res[2])
         msg = await interaction.original_response()
@@ -665,11 +685,12 @@ async def repeat(interaction: discord.Interaction, city:str, province:Optional[s
     if interaction.user.id in bans:
         await interaction.followup.send(":no_pedestrians: You are blocked from using this bot. ")
         return
-    cur.execute('''select chain_end,min_pop,round_number from server_info
+
+    cur.execute('''select chain_end,min_pop from server_info
                 where server_id = ?''', data=(interaction.guild_id,))
-    c=cur.fetchone()[0]  
-    if c:
-        res=search_cities(city,province,country,0)
+    c=cur.fetchone()  
+    if c[0]:
+        res=search_cities(city,province,country,0,c[1])
         if res:
             cur.execute('''select city_id from repeat_info where server_id = ?''', data=(interaction.guild_id,))
             if cur.rowcount>0:
@@ -697,7 +718,11 @@ async def react(interaction: discord.Interaction, city:str, province:Optional[st
     if interaction.user.id in bans:
         await interaction.followup.send(":no_pedestrians: You are blocked from using this bot. ")
         return
-    res=search_cities(city,province,country,0)
+    
+    cur.execute('select min_pop from server_info where server_id=?',data=(interaction.guild_id,))
+    minimum_population = cur.fetchone()[0]
+
+    res=search_cities(city,province,country,0,minimum_population)
     if res:
         try:
             cur.execute('delete from react_info where server_id = ? and city_id = ?', data=(interaction.guild_id,res[0]))
@@ -719,11 +744,12 @@ async def repeat(interaction: discord.Interaction, city:str, province:Optional[s
     if interaction.user.id in bans:
         await interaction.followup.send(":no_pedestrians: You are blocked from using this bot. ")
         return
-    cur.execute('''select chain_end from server_info
+
+    cur.execute('''select chain_end,min_pop from server_info
                 where server_id = ?''', data=(interaction.guild_id,))
-    c=cur.fetchone()[0]  
-    if c:
-        res=search_cities(city,province,country,0)
+    c=cur.fetchone() 
+    if c[0]:
+        res=search_cities(city,province,country,0,c[1])
         if res:
             try:
                 cur.execute('delete from repeat_info where server_id = ? and city_id = ?', data=(interaction.guild_id,res[0]))
@@ -803,7 +829,7 @@ async def chain(message:discord.Message):
             sinfo=cur.fetchone()
             cur.execute('''select city_id from chain_info where server_id = ? and round_number = ? order by count desc''',data=(guildid,sinfo[0]))
             citieslist=[i for (i,) in cur]
-            res=search_cities_chain(message.content[len(sinfo[10]):],0)
+            res=search_cities_chain(message.content[len(sinfo[10]):],0,sinfo[2])
             if res:
                 cur.execute('''select
                         round_number,
@@ -1404,7 +1430,11 @@ async def blocked(interaction:discord.Interaction,se:Optional[Literal['yes','no'
 async def cityinfo(interaction: discord.Interaction, city:str, province:Optional[str]=None, country:Optional[str]=None,se:Optional[Literal['yes','no']]='no'):
     eph=True if se=='no' else False
     await interaction.response.defer(ephemeral=eph)
-    res=search_cities(city,province,country,0)
+
+    cur.execute('select min_pop from server_info where server_id=?',data=(interaction.guild_id,))
+    minimum_population = cur.fetchone()[0]
+
+    res=search_cities(city,province,country,0,minimum_population)
     if res:
         cur.execute("select count from count_info where server_id=? and city_id=?",data=(interaction.guild_id,res[0]))
         if cur.rowcount:
