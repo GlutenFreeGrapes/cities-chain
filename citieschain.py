b7c168ca59385cfd69e3593b8c1ad588a3ecff37
GlutenFreeGrapes <geobee2004@gmail.com>
2024-03-31 10:24:15 -0400

    sorry, fixed block

--- citieschain.py
+++ citieschain.py
@@ -848,7 +848,7 @@ async def on_message(message:discord.Message):
                 processes[guildid]=[(message,guildid,authorid)]
                 await asyncio.create_task(chain(message,guildid,authorid))
         elif message.guild.id==PRANK_SERVER and message.channel.id==channel_id and not message.author.bot:
-            if re.search(r"\b((my bad)|(oop(s?))|(s(o?)(r?)ry))\b",message.content,re.I):
+            if re.search(r"\b((mb)|(my bad)|(oop(s?))|((?<!not.*)s(o?)(r?)ry))\b",message.content,re.I):
                 await message.reply("it's ok")
 async def chain(message:discord.Message,guildid,authorid):
     cur.execute('select user_id from bans where banned=?',data=(True,))
@@ -1700,7 +1700,7 @@ async def ping(interaction: discord.Interaction):
 @tree.command(description="Blocks a user. ")
 @app_commands.check(owner_modcheck)
 async def block(interaction: discord.Interaction,member: discord.Member):
-    if member!=owner and not client.user.bot:
+    if member!=owner and not member.bot:
         cur.execute('select user_id from bans where banned=?',data=(True,))
         bans={i[0] for i in cur}
         if interaction.user.id in bans:
